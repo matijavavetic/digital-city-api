e9884fa615c7a1827b0b9b38fbf6e30e189fd61c
Frano <>
2020-04-17 20:34:50 +0200

    DC-8 Create RoleUpdate integration test

---
+++ digital-city-api/tests/Integration/RoleUpdateTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Tests\Integration;
+
+use Illuminate\Http\Response;
+use src\Applications\Http\Enum\ErrorCodes\RoleErrorCode;
+
+class RoleUpdateTest extends TestCase
+{
+    private string $endpoint = '/api/role.update';
+
+    /**
+     * @test
+     */
+    public function callRoleUpdateEndpointWithValidData_ExpectOkResponse()
+    {
+        // Arrange
+        $listResponse = $this->json('GET', '/api/role.list');
+
+        // get first role from the list
+        $role = $listResponse->json('data.0');
+
+        $data = [
+            'identifier' => $role['identifier'],
+            'name'       => 'New updated role name',
+        ];
+
+        // Act
+        $response = $this->json('POST', $this->endpoint, $data);
+
+        // Assert
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonFragment([
+            'identifier' => $role['identifier'],
+            'name'       => 'New updated role name',
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function callRoleUpdateEndpointWithEmptyIdentifier_ExpectBadRequestResponse()
+    {
+        // Arrange
+        $data = [
+            'identifier' => '',
+        ];
+
+        // Act
+        $response = $this->json('POST', $this->endpoint, $data);
+
+        // Assert
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertJsonFragment([
+            'code' => RoleErrorCode::ERR_EMPTY_IDENTIFIER,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function callRoleUpdateEndpointWithValidEmptyName_ExpectBadRequestResponse()
+    {
+        // Arrange
+        $listResponse = $this->json('GET', '/api/role.list');
+
+        // get first role from the list
+        $role = $listResponse->json('data.0');
+
+        $data = [
+            'identifier' => $role['identifier'],
+            'name'       => '',
+        ];
+
+        // Act
+        $response = $this->json('POST', $this->endpoint, $data);
+
+        // Assert
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertJsonFragment([
+            'code' => RoleErrorCode::ERR_EMPTY_NAME,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function callRoleUpdateEndpointWithInvalidIdentifier_ExpectBadRequestResponse()
+    {
+        // Arrange
+        $data = [
+            'identifier' => 'invalid-uuid-identifier',
+        ];
+
+        // Act
+        $response = $this->json('POST', $this->endpoint, $data);
+
+        // Assert
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertJsonFragment([
+            'code' => RoleErrorCode::ERR_INVALID_IDENTIFIER,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function callRoleUpdateEndpointWithInvalidIdentifierAndName_ExpectBadRequestResponse()
+    {
+        // Arrange
+        $data = [
+            'identifier' => 'invalid-uuid-identifier',
+            'name'       => 'valid-name-but-none-existing-identifier',
+        ];
+
+        // Act
+        $response = $this->json('POST', $this->endpoint, $data);
+
+        // Assert
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertJsonFragment([
+            'code' => RoleErrorCode::ERR_INVALID_IDENTIFIER,
+        ]);
+    }
+}
