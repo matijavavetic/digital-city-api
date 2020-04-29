8dca4dc3c5404e5e49a8588b15bff5caad6a0198
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-04-29 12:47:59 +0200

    DC-39 Tender repo interface and repo

--- src/Data/Repositories/Contracts/ITenderRepository.php
+++ src/Data/Repositories/Contracts/ITenderRepository.php
@@ -6,5 +6,8 @@ use src\Data\Entities\Tender;
 
 interface ITenderRepository
 {
-
+    public function get(string $sort);
+    public function findOne(string $identifier);
+    public function store(Tender $tender);
+    public function destroy(Tender $tender);
 }
