2aaf9a2bd4117bc263fe782bc5434e119d7ecb29
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-05-04 18:11:19 +0200

    DC-52 Delete user_organisation relation

--- src/Data/Entities/Organisation.php
+++ src/Data/Entities/Organisation.php
@@ -15,12 +15,7 @@ class Organisation extends Model
         'county', 'country', 'primary_color',
         'secondary_color', 'tertiary_color', 'logo'
     ];
-
-<<<<<<< Updated upstream
-    public function users()
-    {
-        return $this->belongsToMany(User::class, 'user_organisation');
-=======
+    
     public function city()
     {
         return $this->belongsTo(City::class, 'city_id');
@@ -29,6 +24,5 @@ class Organisation extends Model
     public function county()
     {
         return $this->belongsTo(County::class, 'county_id');
->>>>>>> Stashed changes
     }
 }
