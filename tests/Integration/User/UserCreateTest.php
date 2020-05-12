5866c2c9b7feb09c9f7b91bc6edf84569af1f8d4
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-05-12 15:16:07 +0200

    Update UserCreateTest.php

--- tests/Integration/User/UserCreateTest.php
+++ tests/Integration/User/UserCreateTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Integration\User;
 
 use Illuminate\Http\Response;
+use src\Applications\Http\Enum\ErrorCodes\PermissionErrorCode;
+use src\Applications\Http\Enum\ErrorCodes\UserErrorCode;
 use Tests\Integration\TestCase;
 use Faker\Factory as Faker;
 
@@ -28,5 +30,24 @@ class UserCreateTest extends TestCase
         // Assert
         $response->assertStatus(Response::HTTP_CREATED);
         $response->assertJsonFragment($data);
+    }
+
+    public function callUserCreateEndpointWithInvalidData_ExpectBadRequestResponse()
+    {
+        // Arrange
+        $faker = Faker::create();
+
+        $data = [
+            'email' => $faker->randomDigit,
+        ];
+
+        // Act
+        $response = $this->json('POST', $this->endpoint, $data);
+
+        // Assert
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertJsonFragment([
+            'code' => UserErrorCode::ERR_INVALID_EMAIL,
+        ]);
     }
 }
