00594b5c56b47fc822adb2c275eeaebe6dd6c184
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-05-16 11:23:05 +0200

    DC-59 call organisation.update with valid identifier and invalid name type

--- tests/Integration/Organisation/OrganisationUpdateTest.php
+++ tests/Integration/Organisation/OrganisationUpdateTest.php
@@ -5,6 +5,7 @@ namespace Tests\Integration\Organisation;
 use Illuminate\Http\Response;
 use src\Applications\Http\Enum\ErrorCodes\OrganisationErrorCode;
 use Tests\Integration\TestCase;
+use Faker\Factory as Faker;
 
 class OrganisationUpdateTest extends TestCase
 {
@@ -55,6 +56,35 @@ class OrganisationUpdateTest extends TestCase
         $response->assertStatus(Response::HTTP_BAD_REQUEST);
         $response->assertJsonFragment([
             'code' => OrganisationErrorCode::ERR_EMPTY_IDENTIFIER,
+        ]);
+    }
+
+
+    /**
+     * @test
+     */
+    public function callOrganisationUpdateEndpointWithValidIdentifierAndInvalidNameDataType_ExpectBadRequestResponse()
+    {
+        // Arrange
+        $listResponse = $this->json('GET', '/api/organisation.list');
+
+        $faker = Faker::create();
+
+        // Get first organisation from the list
+        $organisation = $listResponse->json('data.0');
+
+        $data = [
+            'identifier' => $organisation['identifier'],
+            'name'       => $faker->randomDigit,
+        ];
+
+        // Act
+        $response = $this->json('POST', $this->endpoint, $data);
+
+        // Assert
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertJsonFragment([
+            'code' => OrganisationErrorCode::ERR_INVALID_NAME,
         ]);
     }
 }
