0fb1d26c76f0ad6fbac9789741fca2526bf02e96
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-05-16 11:31:00 +0200

    DC-59 role.info valid data test

---
+++ tests/Integration/Role/RoleInfoTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Integration\Role;
+
+use Illuminate\Http\Response;
+use src\Applications\Http\Enum\ErrorCodes\RoleErrorCode;
+use Tests\Integration\TestCase;
+use Faker\Factory as Faker;
+
+class RoleInfoTest extends TestCase
+{
+    private string $endpoint = '/api/role.info';
+
+    /**
+     * @test
+     */
+    public function callRoleInfoEndpointWithValidData_ExpectOkResponse()
+    {
+        // Arrange
+        $listResponse = $this->json('GET', '/api/role.list');
+
+        // Get first role from the list
+        $role = $listResponse->json('data.0');
+
+        $data = [
+            'identifier' => $role['identifier'],
+        ];
+
+        // Act
+        $response = $this->json('POST', $this->endpoint, $data);
+
+        // Assert
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonFragment([
+            'identifier' => $role['identifier'],
+            'name'       => $role['name']
+        ]);
+    }
+}
