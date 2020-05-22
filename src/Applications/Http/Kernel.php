ceb8d30edfab1c1eae0c7aa71ce079fbe31d4ab5
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-05-22 13:06:41 +0200

    Update Kernel.php

--- src/Applications/Http/Kernel.php
+++ src/Applications/Http/Kernel.php
@@ -5,17 +5,20 @@ namespace src\Applications\Http;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 use src\Applications\Http\Middleware\Authenticate;
 use src\Applications\Http\Middleware\Authorization;
-use src\Applications\Http\Middleware\Cors;
 use src\Applications\Http\Middleware\Jsonify;
+use Fruitcake\Cors\HandleCors as Cors;
 
 class Kernel extends HttpKernel
 {
+    protected $middleware = [
+        Cors::class
+    ];
+
     protected $middlewareGroups = [
         'api' => [
             Jsonify::class,
             Authorization::class,
             Authenticate::class,
-            Cors::class
         ],
     ];
 }
